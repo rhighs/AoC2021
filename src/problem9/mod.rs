ec2e2b57490a970e7f68154a2391be1cf90a3e01
roberto montalti <roberto.montalti00@gmail.com>
2021-12-09 23:08:33 +0100

    Problem 9 part 2

--- src/problem9/mod.rs
+++ src/problem9/mod.rs
@@ -8,12 +8,12 @@ fn make_grid(data: &Vec<String>) -> Vec<Vec<u32>> {
         grid.push(line)
     }
     for row in &mut grid {
-        row.insert(0, 10);
-        row.push(10);
+        row.insert(0, 9);
+        row.push(9);
     }
     let mut padding = Vec::new();
     for _ in 0..grid[0].len() {
-        padding.push(10);
+        padding.push(9);
     }
     grid.insert(0, padding.clone());
     grid.push(padding);
@@ -38,5 +38,68 @@ pub fn p9_1(data: &Vec<String>) -> u32 {
 }
 
 pub fn p9_2(data: &Vec<String>) -> u32 {
-    0
+    let mut grid = make_grid(data);
+    let mut basins = Vec::new();
+
+    loop {
+        let mut q = Vec::new();
+        let mut point_found = false;
+        for i in 1..(grid.len() - 1) {
+            for j in 1..(grid[0].len() - 1) {
+                if grid[i][j] != 9 {
+                    q.push((i, j));
+                    point_found = true;
+                    break;
+                }
+            }
+            if point_found {
+                break;
+            }
+        }
+
+        if q.len() == 0 {
+            break;
+        }
+
+        let mut values = Vec::from([grid[q[0].0][q[0].1]]);
+        while q.len() > 0 {
+            let pos = q[0];
+            let up = grid[pos.0 - 1][pos.1];
+            let down = grid[pos.0 + 1][pos.1];
+            let left = grid[pos.0][pos.1 - 1];
+            let right = grid[pos.0][pos.1 + 1];
+
+            if up != 9 {
+                q.push((pos.0 - 1, pos.1));
+                grid[pos.0 - 1][pos.1] = 9;
+                values.push(up);
+            }
+            if down != 9 {
+                q.push((pos.0 + 1, pos.1));
+                grid[pos.0 + 1][pos.1] = 9;
+                values.push(down);
+            }
+            if left != 9 {
+                q.push((pos.0, pos.1 - 1));
+                grid[pos.0][pos.1 - 1] = 9;
+                values.push(left);
+            }
+            if right != 9 {
+                q.push((pos.0, pos.1 + 1));
+                grid[pos.0][pos.1 + 1] = 9;
+                values.push(right);
+            } 
+
+            grid[pos.0][pos.1] = 9;
+            q.remove(0);
+        }
+
+        basins.push(values);
+    }
+
+    basins.sort_by(|a, b| a.len().cmp(&b.len()));
+    let last = basins[basins.len() - 1].len();
+    let last1 = basins[basins.len() - 2].len();
+    let last2 = basins[basins.len() - 3].len();
+    (last * last1 * last2) as u32
 }
