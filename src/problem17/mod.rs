d65eac71a508e233bb127a3ffd757a4ca65791d5
roberto montalti <roberto.montalti00@gmail.com>
2021-12-20 22:56:36 +0100

    Problem 17 part 1

--- src/problem17/mod.rs
+++ src/problem17/mod.rs
@@ -8,23 +8,37 @@ fn get_target(line: String) -> ((i32, i32), (i32, i32)) {
         .iter()
         .map(|value| value.parse::<i32>().unwrap())
         .collect::<Vec<i32>>();
-    let y_coord = split_coord[1][2..split_coord[0].len() - 1]
+    let y_coord = split_coord[1][2..split_coord[1].len()]
         .to_string()
         .split("..")
         .collect::<Vec<&str>>()
         .iter()
         .map(|value| value.parse::<i32>().unwrap())
         .collect::<Vec<i32>>();
-
     ((x_coord[0], x_coord[1]), (y_coord[0], y_coord[1]))
 }
 
-pub fn p17_1(data: &Vec<String>) -> u32 {
-    println!("{:?}", get_target(data[0].clone()));
-    0
+fn inv_gauss(m: i32) -> bool {
+    let n = ((2 * m) as f32 + 0.25f32).sqrt() - 0.5f32;
+    n.fract() == 0.0f32
 }
 
-pub fn p17_2(data: &Vec<String>) -> u32 {
-    0
+pub fn p17_1(data: &Vec<String>) -> i32 {
+    let (x_range, y_range) = get_target(data[0].clone());
+    let mut x_speed = 0;
+    let y_speed = (y_range.0 + 1) * -1;
+
+    for i in x_range.0..x_range.1 {
+        if inv_gauss(i) {
+            x_speed = i;
+        }
+    }
+
+    println!("Starting velocity: {:?}", (x_speed, y_speed));
+    (0..y_speed + 1).collect::<Vec<_>>().iter().sum()
+}
+
+pub fn p17_2(data: &Vec<String>) -> (i32, i32) {
+    (0, 0)
 }
 
