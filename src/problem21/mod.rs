887fa295a398bc3f722fc6a0138afe33a2991d14
Roberto Montalti <roberto.montalti00@gmail.com>
2022-02-27 23:41:37 +0100

    Solved part 2, copy makes things a bit slow ...who cares

--- src/problem21/mod.rs
+++ src/problem21/mod.rs
@@ -47,6 +47,91 @@ pub fn p21_1(data: &Vec<String>) -> u32 {
     loser_score * rolls
 }
 
-pub fn p21_2(data: &Vec<String>) -> u32 {
-    0
+const FREQ: [u128; 7] = [1, 3, 6, 7, 6, 3, 1];
+
+#[derive(Clone, Copy)]
+struct Universe {
+    p1_pos: u32,
+    p2_pos: u32,
+    p1_score: u32,
+    p2_score: u32,
+    nsimilar: u128,
+    turn: bool
+}
+
+impl Universe {
+    fn new(p1_pos: u32, p2_pos: u32) -> Self {
+        Self {
+            p1_pos: p1_pos,
+            p2_pos: p2_pos,
+            p1_score: 0,
+            p2_score: 0,
+            nsimilar: 1,
+            turn: true
+        }
+    }
+
+    fn check_winner(&self) -> Option<bool> {
+        if self.p1_score >= 21 {
+            return Some(true);
+        }
+
+        if self.p2_score >= 21 {
+            return Some(false);
+        }
+
+        None
+    }
+
+    fn move_player(&mut self, rollvalue: u32) {
+        if self.turn {
+            self.p1_pos = movepawn(rollvalue, self.p1_pos);
+            self.p1_score += self.p1_pos;
+        } else {
+            self.p2_pos = movepawn(rollvalue, self.p2_pos);
+            self.p2_score += self.p2_pos;
+        }
+
+        self.turn = !self.turn;
+    }
+}
+
+#[derive(Debug)]
+struct Multiverse {
+    p1_wins: u128,
+    p2_wins: u128
+}
+
+impl Multiverse {
+    fn winner_score(&self) -> u128 {
+        if self.p1_wins > self.p2_wins {self.p1_wins} else {self.p2_wins}
+    }
+}
+
+fn multiverse(universe: Universe, m: &mut Multiverse) {
+    for (i, freq) in FREQ.iter().enumerate() {
+        let rollvalue: u32 = i as u32 + 3;
+        let mut u = universe.clone();
+        u.move_player(rollvalue);
+
+        if let Some(p1_won) = u.check_winner() {
+            if p1_won {
+                m.p1_wins += *freq * u.nsimilar;
+            } else {
+                m.p2_wins += *freq *  u.nsimilar;
+            }
+        } else {
+            u.nsimilar *= freq;
+            multiverse(u, m);
+        }
+    }
+}
+
+pub fn p21_2(data: &Vec<String>) -> u128 {
+    let (p1_pos, p2_pos) = positions(data);
+    let mut m = Multiverse { p1_wins: 0, p2_wins: 0 };
+    let u = Universe::new(p1_pos, p2_pos);
+
+    multiverse(u, &mut m);
+    m.winner_score()
 }
